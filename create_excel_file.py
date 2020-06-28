49a687c21189df32f099c87cbccd4b29b10cc239
Neel Jotani <57675751+neeljotani@users.noreply.github.com>
2020-06-28 18:26:47 +0100

    Update create_excel_file.py

--- create_excel_file.py
+++ create_excel_file.py
@@ -5,9 +5,6 @@ from pytz import timezone
 from datetime import datetime
 import time
 
-
-
-
 India = timezone('Asia/Kolkata')
 ind_time = datetime.now(India)
 date1 = ind_time.strftime("%d_%m_%Y")
@@ -17,6 +14,17 @@ print(hour == 10)
 src_dir="Report Format/Format.xlsx"
 dst_dir="Bluesr_New_Prod_Server_hourly_report_"+date1+".xlsx"
 if hour == 10:
+	
+	
+	
+	
+	
+	
+	
+	
+
+
+
 	shutil.copy(src_dir,dst_dir)
 else:
 	print("Not the first report")
